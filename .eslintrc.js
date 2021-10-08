34f5f9893ca25431d604a41ead2f7d87b2552fc5
medvosa <38169798+medvosa@users.noreply.github.com>
2021-10-09 00:52:19 +0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/airbnb',
+    '@vue/typescript/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    "linebreak-style": 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-blocks': ["off"],
+    'no-trailing-spaces': ["off"],
+    'indent': ["off"],
+    'comma-dangle': ["off"],
+  },
+};
